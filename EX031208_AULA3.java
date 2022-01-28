ac5f46573b655d80c043da960d462dee5fc0a7f5
Jennifer Sousa <jennifergaldinodesousa@gmail.com>
2022-01-28 20:54:38 -0300

    2801

---
+++ EX031208_AULA3.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EX031208_AULA3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //A = 10, B = 7, C = A-B, D = C+A, E = D+C. Crie um programa em Java para resolver as equaçoes acima
+        
+        double a,b,c,d,e;
+        
+        a = 10;
+        b = 7;
+        c = a - b;
+        d = c + a;
+        e = d + c;
+        
+        System.out.print(e);
+        
+        
+        
+    }
+    
+}
